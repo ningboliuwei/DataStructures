7f2fe3ace957ffda5810eb143113777158a7115a
LIU Wei <liuwei@nbu.edu.cn>
2018-09-17 12:15:39 +0800

    change 2 office

--- Chapter4/SequenceQueue/SequenceQueue_1.c
+++ Chapter4/SequenceQueue/SequenceQueue_1.c
@@ -7,20 +7,20 @@
 
 #define MAX_LENGTH 10
 
-typedef struct {
+typedef struct SequenceQueueType {
     int data[MAX_LENGTH];
     int front;
     int rear;
 } *SequenceQueue;
 
-int IsEmptyQueue(SequenceQueue *queue) {
+int IsEmptyQueue(SequenceQueue queue) {
     if (queue->front == -1 && queue->rear == -1) {
         return 1;
     }
     return 0;
 }
 
-int LengthOfQueue(SequenceQueue *queue) {
+int LengthOfQueue(SequenceQueue queue) {
     if (IsEmptyQueue(queue)) {
         return 0;
     } else {
@@ -29,34 +29,60 @@ int LengthOfQueue(SequenceQueue *queue) {
 }
 
 int InQueue(SequenceQueue *queue, int item) {
-    if ()
-
-    if (IsEmptyQueue(queue)) {
-        queue->front = 0;
+    if ((*queue)->rear == MAX_LENGTH - 1) {
+        return 0;
     }
 
-    queue->rear++;
-    queue->data[queue->rear] = item;
+    if (IsEmptyQueue(queue)) {
+        (*queue)->front = 0;
+    }
+
+    (*queue)->rear++;
+    (*queue)->data[(*queue)->rear] = item;
 
     return 1;
 }
 
-int OutQueue(SequenceQueue *queue,
+int OutQueue(SequenceQueue *queue, int *item) {
+    if (IsEmptyQueue(queue)) {
+        return 0;
+    }
 
-*
-int item
-){
-
+    *item = (*queue)->data[(*queue)->front];
+    return 1;
 }
 
 int InitQueue(SequenceQueue *queue) {
-    queue->front = -1;
-    queue->tail = -1;
+    (*queue)->front = -1;
+    (*queue)->rear = -1;
+}
+
+void ShowQueue(SequenceQueue queue) {
+    printf("items: ");
+    for (int i = queue->front; i <= queue->rear; i++) {
+        printf("%d ", queue->data[i]);
+    }
+    printf("\n");
 }
 
 int main() {
     SequenceQueue *queue = (SequenceQueue *) malloc(sizeof(SequenceQueue));
     InitQueue(queue);
 
+    InQueue(queue, 1);
+    InQueue(queue, 2);
+    InQueue(queue, 3);
+    InQueue(queue, 4);
+
+    ShowQueue(queue);
+
+    int *item = (int *) malloc(sizeof(int));
+
+    OutQueue(queue, item);
+    printf("out: %d", *item);
+    OutQueue(queue, item);
+    printf("out: %d", *item);
+
+    ShowQueue(queue);
 
 }
