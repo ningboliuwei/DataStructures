918f75ad074c4e34b35b0234c4dc44a36b650365
ningboliuwei <liuwei@nbu.edu.cn>
2018-09-17 15:16:17 +0800

    搞定链表队列

---
+++ Chapter4/LinkQueue/LinkQueue_1.c
@@ -0,0 +1,45 @@
+//
+// Created by Liu Wei on 2018/9/17.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LENGTH 10
+
+typedef struct SequenceQueueType {
+    int data[MAX_LENGTH];
+    int front;
+    int rear;
+} *SequenceQueue;
+
+int IsEmptyQueue(SequenceQueue queue) {
+
+}
+
+int LengthOfQueue(SequenceQueue queue) {
+
+}
+
+int InQueue(SequenceQueue *queue, int item) {
+
+}
+
+int OutQueue(SequenceQueue *queue, int *item) {
+
+}
+
+int InitQueue(SequenceQueue *queue) {
+
+}
+
+void ShowQueue(SequenceQueue queue) {
+
+}
+
+int main() {
+
+
+    getchar();
+
+}
