6a7d2d66dbc510ec7ff0fa6d8e4a200abe4b4656
LIU Wei <liuwei@nbu.edu.cn>
2018-09-19 22:32:15 +0800

    working on 顺序串（again ？）

--- Chapter5_String/SequenceString/SequenceString_1.c
+++ Chapter5_String/SequenceString/SequenceString_1.c
@@ -2,3 +2,21 @@
 // Created by 慰 on 2018/9/19.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LENGTH 100
+
+typedef struct SequenceStringStruct {
+    char vec[MAX_LENGTH];
+    int length;
+} SequenceString;
+
+int Length(SequenceString *s){
+    return s->length;
+}
+
+int Concact(SequenceString *s, SequenceString *t){
+    
+}
+
