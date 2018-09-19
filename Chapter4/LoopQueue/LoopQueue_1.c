6864e92a3a035721e869f127c943d4325cc711ff
LIU Wei <liuwei@nbu.edu.cn>
2018-09-19 22:05:50 +0800

    搞定循环队列

--- Chapter4/LoopQueue/LoopQueue_1.c
+++ Chapter4/LoopQueue/LoopQueue_1.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_LENGTH 10
+#define MAX_LENGTH 7
 
 typedef struct LoopQueueType {
     int data[MAX_LENGTH];
@@ -26,18 +26,18 @@ int IsEmptyQueue(LoopQueue queue) {
 }
 
 int LengthOfQueue(LoopQueue queue) {
-    int length = (queue->rear - queue->front + MAX_LENGTH) % MAX_LENGTH;
+    int length = (queue->rear + MAX_LENGTH - queue->front) % MAX_LENGTH;
 
     return length;
 }
 
 int InQueue(LoopQueue *queue, int item) {
     // 队列已满
-    if ((*queue)->front == (*queue)->rear + 1) {
+    if ((*queue)->front == ((*queue)->rear + 1) % MAX_LENGTH) {
         return 0;
     }
 
-    (*queue)->rear++;
+    (*queue)->rear = ((*queue)->rear + 1) % MAX_LENGTH;
     (*queue)->data[(*queue)->rear] = item;
 
     return 1;
@@ -48,8 +48,8 @@ int OutQueue(LoopQueue *queue, int *item) {
         return 0;
     }
 
+    (*queue)->front = ((*queue)->front + 1) % MAX_LENGTH;
     *item = (*queue)->data[(*queue)->front];
-    (*queue)->front++;
 
     return 1;
 }
@@ -65,33 +65,107 @@ int InitQueue(LoopQueue *queue) {
 void ShowQueue(LoopQueue queue) {
     printf("items: ");
 
-    for (int i = queue->front; i <= queue->rear; i++) {
-        printf("%d ", queue->data[i]);
+    int start = queue->front + 1;
+    int length = LengthOfQueue(queue);
+
+    for (int i = start; i < start + length; i++) {
+        printf("%d ", queue->data[i % MAX_LENGTH]);
     }
     printf("\n");
 }
 
+void ShowLength(LoopQueue queue) {
+    printf("length of queue: %d\n", LengthOfQueue(queue));
+    printf("-----------------\n");
+}
+
 int main() {
     LoopQueue *queue = (LoopQueue *) malloc(sizeof(LoopQueue));
-    InitQueue(queue);
-
-    InQueue(queue, 1);
-    InQueue(queue, 2);
-    InQueue(queue, 3);
-    InQueue(queue, 4);
-
-    ShowQueue(*queue);
     int *item = (int *) malloc(sizeof(int));
 
-    OutQueue(queue, item);
-    printf("out: %d\n", *item);
+    InitQueue(queue);
+
     ShowQueue(*queue);
+    ShowLength(*queue);
+
+    InQueue(queue, 1);
+    printf("in: 1\n");
+    ShowQueue(*queue);
+    ShowLength(*queue);
+
+    InQueue(queue, 2);
+    printf("in: 2\n");
+    ShowQueue(*queue);
+    ShowLength(*queue);
+
 
     OutQueue(queue, item);
     printf("out: %d\n", *item);
     ShowQueue(*queue);
+    ShowLength(*queue);
 
-    printf("length of queue: %d", LengthOfQueue(*queue));
+    OutQueue(queue, item);
+    printf("out: %d\n", *item);
+    ShowQueue(*queue);
+    ShowLength(*queue);
+
+    InQueue(queue, 3);
+    printf("in: 3\n");
+    ShowQueue(*queue);
+    ShowLength(*queue);
+
+    InQueue(queue, 4);
+    printf("in: 4\n");
+    ShowQueue(*queue);
+    ShowLength(*queue);
+
+    OutQueue(queue, item);
+    printf("out: %d\n", *item);
+    ShowQueue(*queue);
+    ShowLength(*queue);
+
+
+    OutQueue(queue, item);
+    printf("out: %d\n", *item);
+    ShowQueue(*queue);
+    ShowLength(*queue);
+
+    for (int i = 0; i < 5; i++) {
+        int newItem = i + 5;
+        InQueue(queue, newItem);
+        printf("in: %d\n", newItem);
+        ShowQueue(*queue);
+        ShowLength(*queue);
+    }
+
+    InQueue(queue, 10);
+    printf("in: 10\n");
+    ShowQueue(*queue);
+    ShowLength(*queue);
+
+    for (int i = 0; i < 4; i++) {
+        OutQueue(queue, item);
+        printf("out: %d\n", *item);
+        ShowQueue(*queue);
+        ShowLength(*queue);
+    }
+
+    for (int i = 0; i < 4; i++) {
+        int newItem = i + 11;
+        InQueue(queue, newItem);
+        printf("in: %d\n", newItem);
+        ShowQueue(*queue);
+        ShowLength(*queue);
+    }
+
+    int count = LengthOfQueue(*queue);
+    
+    for (int i = 0; i < count; i++) {
+        OutQueue(queue, item);
+        printf("out: %d\n", *item);
+        ShowQueue(*queue);
+        ShowLength(*queue);
+    }
 
     getchar();
 
