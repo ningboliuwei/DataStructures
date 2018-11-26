86098cb1c10fdd0634c0be0321fa4442010f6be4
ningboliuwei <liuwei@nbu.edu.cn>
2018-11-26 09:20:12 +0800

    搞定 11-1

--- Chapter8_Graph/OJ/11-1.cpp
+++ Chapter8_Graph/OJ/11-1.cpp
@@ -3,6 +3,7 @@
 // 输入顶点和边，创建无向网的邻接矩阵
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAXLEN 10
 typedef struct {
@@ -14,54 +15,58 @@ typedef struct {
     int e;
 } MGraph;
 
-void CreateGraph(MGraph g) {
+void CreateGraph(MGraph *g) {
     char vex1;
     char vex2;
-    char weight;
+    int weight;
     int pos1;
     int pos2;
 
-    scanf("%d %d", &(g.n), &(g.e));
 
-    for (int i = 0; i < g.n; i++) {
-        scanf("\n%c", &g.vexs[i]);
+    for (int i = 0; i < MAXLEN; i++) {
+        for (int j = 0; j < MAXLEN; j++) {
+            g->edges[i][j] = 0;
+        }
     }
 
-    for (int i = 0; i < g.e; i++) {
-        scanf("\n");
-        scanf("%c", &vex1);
-        scanf(" ");
-        scanf("%c", &vex2);
-        scanf(" ");
-        scanf("%d", &weight);
+    scanf("%d %d\n", &(g->n), &(g->e));
 
-        for (pos1 = 0; pos1 < g.n; pos1++) {
-            if (g.vexs[pos1] == vex1) {
+    for (int i = 0; i < g->n; i++) {
+        char c;
+        scanf("\n%c", &c);
+        g->vexs[i] = c;
+    }
+
+    for (int i = 0; i < g->e; i++) {
+        scanf("\n%c %c %d", &vex1, &vex2, &weight);
+
+        for (pos1 = 0; pos1 < g->n; pos1++) {
+            if (g->vexs[pos1] == vex1) {
                 break;
             }
         }
 
-        for (pos2 = 0; pos2 < g.n; pos2++) {
-            if (g.vexs[pos2] == vex2) {
+        for (pos2 = 0; pos2 < g->n; pos2++) {
+            if (g->vexs[pos2] == vex2) {
                 break;
             }
         }
 
-        g.edges[pos1][pos2] = weight;
+        g->edges[pos1][pos2] = weight;
     }
 }
 
-void DisplayGraph(MGraph g) {
-    for (int i = 0; i < g.n; i++) {
-        for (int j = 0; j < g.n; j++) {
-            printf("%d", g.edges[i][j]);
+void DisplayGraph(MGraph *g) {
+    for (int i = 0; i < g->n; i++) {
+        for (int j = 0; j < g->n; j++) {
+            printf("%d", g->edges[i][j]);
         }
         printf("\n");
     }
 }
 
 int main() {
-    MGraph g;
+    MGraph *g = (MGraph *)malloc(sizeof(MGraph));
 
     CreateGraph(g);
     DisplayGraph(g);
