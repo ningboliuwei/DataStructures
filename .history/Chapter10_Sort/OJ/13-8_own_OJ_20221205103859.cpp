1d3daeb308a3818c8ab0db5049ec53457f2ce3e0
刘慰 <ningboliuwei@live.cn>
2022-12-05 16:18:24 +0800

    终于搞定OJ大根堆排序

---
+++ .history/Chapter10_Sort/OJ/13-8_own_OJ_20221205103859.cpp
@@ -0,0 +1,45 @@
+//
+// Created by 慰 on 2018/12/13.
+// 问题 G: 【数据结构10-8】堆排序
+// ID: 1663
+// 输入数据：
+// 5 7 4 3 8 1 6 9 2 -1
+// 5 7 3 4 1 8 6 9 2
+// 3 4 5 7 1 6 8 9 2
+// 1 3 4 5 6 7 8 9 2
+// 1 2 3 4 5 6 7 8 9
+#include <stdio.h>
+#include <math.h>
+#define MAX_LENGTH 20
+
+void InputArray(int array[], int length) {
+    int number;
+    // 注意这里下标从 1 开始
+    for (int i = 1; i <= length; i++) {
+        scanf("%d", &number);
+        array[i] = number;
+    }
+}
+
+void ShowArray(int array[], int length) {
+    // 注意这里下标从 1 开始
+    for (int i = 1; i <= length; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void MergeSort(int array1[], int array2[], int pos1, int pos2) {
+}
+
+int main() {
+    int array[MAX_LENGTH];
+    int count = 0;
+
+    scanf("%d", &count);
+    InputArray(array, count);
+    MergeSort(array, count);
+
+    getchar();
+    getchar();
+}
